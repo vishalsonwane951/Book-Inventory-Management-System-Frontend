7f737c6fb66ea30297a69a7bb5432748810cce2e
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-01-24 13:11:48 +0530

    api

--- src/services/bookApi.js
+++ src/services/bookApi.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
-export const getAllBooks = () => api.get("/allbooks");
+export const getAllBooks = () => api.get("/");
 export const getBookById = (id) => api.get(`/books/${id}`);
 export const addBook = (data) => api.post("/createbook", data);
 export const updateBook = (id, data) => api.put(`/update-book/${id}`, data);
