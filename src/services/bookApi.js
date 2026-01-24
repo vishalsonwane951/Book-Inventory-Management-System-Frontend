3e05b992fccd84d14d39a0d9adb6868f89dc3320
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-01-24 07:34:57 +0530

    add backend server

--- src/services/bookApi.js
+++ src/services/bookApi.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URI = process.env.API_URL
 
-export const getAllBooks = () => axios.get(`${API_URL}/allbooks`);
-export const getBookById = (id) => axios.get(`${API_URL}/books/${id}`);
-export const addBook = (data) => axios.post(`${API_URL}/createbook`, data);
-export const updateBook = (id, data) => axios.put(`${API_URL}/update-book/${id}`, data);
-export const deleteBook = (id) => axios.delete(`${API_URL}/delete-book/${id}`);
+export const getAllBooks = () => axios.get(`${API_URI}/allbooks`);
+export const getBookById = (id) => axios.get(`${API_URI}/books/${id}`);
+export const addBook = (data) => axios.post(`${API_URI}/createbook`, data);
+export const updateBook = (id, data) => axios.put(`${API_URI}/update-book/${id}`, data);
+export const deleteBook = (id) => axios.delete(`${API_URI}/delete-book/${id}`);
